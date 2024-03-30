1869630c2f98a10deebb442b107130d6a01b8f4d
rakicodes <goraquel22@gmail.com>
2024-03-30 13:23:18 -0600

    fix user url

--- web/src/features/user/userService.js
+++ web/src/features/user/userService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const { REACT_APP_SERVER } = process.env;
 
-const API_URL = `${REACT_APP_SERVER}/api/recipes`
+const API_URL = `${REACT_APP_SERVER}/api/users`
 
 const getUser = async (id) => {
 	const response = await axios.get(`${API_URL}/${id}`);
